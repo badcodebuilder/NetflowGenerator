95221e30ffd6627e8b29d23934f3c1b806682877
BadCodeBuilder <callmechinese@163.com>
2020-11-04 14:36:52 +0800

    init push

---
+++ config.h
@@ -0,0 +1,7 @@
+#define     MAX_SENDER_THREAD_CNT   50
+#define     MAX_RECVER_THREAD_CNT   20
+
+#define     SRV_ADDR                "192.168.1.99"
+#define     SRV_PORT                55667
+
+#define     SEND_INTERVAL           100

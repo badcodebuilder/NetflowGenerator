cce8d578335ca72157db6edb006e61338593b931
BadCodeBuilder <callmechinese@163.com>
2020-11-07 18:52:39 +0800

    add plot script and redis configuration file

---
+++ scripts/flowplot.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+
+import matplotlib.pyplot as plt
+import redis
+
+def main()->int:
+    """
+        Main Function
+        ===
+    """
+    # Get all data in form of List[string]
+    conn = redis.Redis(host="localhost", port=6379, decode_responses=True)
+    lenth = conn.llen("packet")
+    rdata = conn.lrange("packet", 0, lenth)
+    conn.close()
+    print("Read %d items from redis"%(len(rdata)))
+
+    # Convert data from string to int
+    data = []
+    for i in rdata:
+        data.append(int(i))
+
+    # Count data
+    interval = 200
+    maxTime = max(data)
+    slices = maxTime//interval
+    count = [0]*(slices + 1)
+    for i in data:
+        count[i//interval] += 1
+
+    # Plot data
+    plt.xlabel("time")
+    plt.ylabel("count")
+    plt.plot(count)
+    plt.savefig("data.png")
+    #plt.show()
+
+if __name__ == "__main__":
+    main()
+    pass
